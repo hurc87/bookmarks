2ed2b7715133e0046ba64df1611fba03892dc246
hurc87 <zoe1987@btinternet.com>
2018-10-25 09:08:31 +0100

    chapter 12

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -16,9 +16,13 @@ class Bookmark
     else
       connection = PG.connect(dbname: 'bookmark_manager')
     end
-    result = connection.exec("SELECT * FROM bookmarks;")
-    result.map do |bookmark|
-      Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url'])
+    bookmarks = connection.exec('SELECT * FROM bookmarks;')
+    bookmarks.map do |bookmark|
+      Bookmark.new(
+        id: bookmark['id'],
+        title: bookmark['title'],
+        url: bookmark['url']
+      )
     end
   end
 
@@ -32,5 +36,14 @@ class Bookmark
     result = connection.exec("INSERT INTO bookmarks (url, title) VALUES('#{url}', '#{title}') RETURNING id, title, url;")
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
+
+    def self.delete(id:)
+      if ENV['ENVIRONMENT'] == 'test'
+        connection = PG.connect(dbname: 'bookmark_manager_test')
+      else
+        connection = PG.connect(dbname: 'bookmark_manager')
+      end
+     connection.exec("DELETE FROM bookmarks WHERE id = #{id}")
+   end
 
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -5,7 +5,6 @@ require 'database_helpers'
 describe Bookmark do
   describe '.all' do
     it 'returns all bookmarks' do
-      connection = PG.connect(dbname: 'bookmark_manager_test')
 
       bookmark = Bookmark.create(url: "http://www.makersacademy.com", title: "Makers Academy")
       Bookmark.create(url: "http://www.destroyallsoftware.com", title: "Destroy All Software")
@@ -20,16 +19,26 @@ describe Bookmark do
       expect(bookmarks.first.url).to eq 'http://www.makersacademy.com'
     end
   end
-end
 
-describe '.create' do
-  it 'creates a new bookmark' do
-    bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark')
-    persisted_data = persisited_data(id: bookmark.id)
+  describe '.create' do
+    it 'creates a new bookmark' do
+      bookmark = Bookmark.create(url: 'http://www.testbookmark.com', title: 'Test Bookmark')
+      persisted_data = persisited_data(id: bookmark.id)
 
-    expect(bookmark).to be_a Bookmark
-    expect(bookmark.id).to eq persisted_data.first['id']
-    expect(bookmark.title).to eq 'Test Bookmark'
-    expect(bookmark.url).to eq 'http://www.testbookmark.com'
+      expect(bookmark).to be_a Bookmark
+      expect(bookmark.id).to eq persisted_data.first['id']
+      expect(bookmark.title).to eq 'Test Bookmark'
+      expect(bookmark.url).to eq 'http://www.testbookmark.com'
+    end
+  end
+
+  describe '.delete' do
+    it 'deletes the given bookmark' do
+      bookmark = Bookmark.create(title: 'Makers Academy', url: 'http://www.makersacademy.com')
+
+      Bookmark.delete(id: bookmark.id)
+
+      expect(Bookmark.all.length).to eq 0
+    end
   end
 end
